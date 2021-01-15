8078057f0779b4c999b3e548462ccc67a7fde093
Ivan <akbars06_2010@mail>
2021-01-15 18:42:14 +0300

    add List, weekly

--- src/components/weekly.js
+++ src/components/weekly.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
+import WeeklyForm from './weeklyForm';
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit} from 'react-icons/ti';
 
-function weekly() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Weekly({lists, completeList}) {
+
+    const [edit, setEdit]  = useState({
+        id: null,
+        value: ''
+    })
+
+    return lists.map((list, i) => {
+        return <div className={list.isComplete ? 'list-row complete' : 'list-row'}
+            key={i}>
+                <div key={list.id} onClick={() => completeList(list.id)}>
+                    {list.text}
+                </div>
+                <div className='icons'>
+                    <RiCloseCircleLine/>
+                    <TiEdit/>
+                </div>
+            </div>
+    });
 }
 
-export default weekly
+export default Weekly
--- src/components/weeklyList.js
+++ src/components/weeklyList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import WeeklyForm from './weeklyForm';
+import Weekly from './weekly';
 
 function WeeklyList() {
     const [lists, setList] = useState([]);
@@ -12,12 +13,26 @@ function WeeklyList() {
         const newList = [list, ...lists]
 
         setList(newList); 
+    };
+
+    const completeList = (id) => {
+        let upLists = lists.map(list => {
+            if (list.id === id) {
+                list.isComplete = !list.isComplete
+            }
+            return list;
+        })
+        setList(upLists);
     }
 
     return (
         <div>
            <h1>What's the Plan for Today</h1> 
            <WeeklyForm onSubmit={addList}/>
+           <Weekly
+            lists = {lists}
+            completeList = {completeList}
+           />
         </div>
     )
 }
